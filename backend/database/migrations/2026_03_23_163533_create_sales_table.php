238f94fd3295cfe41ede76eb1873584309a6dc13
yurest <yurest@Mac-mini-de-yurest.local>
2026-03-23 17:58:58 +0100

    Primeras configuraciones de la BBDD

---
+++ backend/database/migrations/2026_03_23_163533_create_sales_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid');
+            $table->integer('ticket_number');
+            $table->string('status');
+            $table->integer('diners');
+            $table->timestamp('opened_at');
+            $table->timestamp('closed_at');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sales');
+    }
+};
