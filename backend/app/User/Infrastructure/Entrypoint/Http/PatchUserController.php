60a9e918b12c0c8dad69a5f44a476a06db7828a0
Laura Mora <l.mora@yurest.com>
2026-04-02 13:49:24 +0200

    Archivos endpoint de user actualizados

--- backend/app/User/Infrastructure/Entrypoint/Http/PatchUserController.php
+++ backend/app/User/Infrastructure/Entrypoint/Http/PatchUserController.php
@@ -6,7 +6,7 @@ use App\User\Application\UpdateUser\UpdateUser;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
-class PatchController
+class PatchUserController
 {
     public function __construct(
         private UpdateUser $updateUser
