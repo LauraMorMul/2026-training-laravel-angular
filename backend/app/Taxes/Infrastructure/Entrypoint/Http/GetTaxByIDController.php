f3218937f7205c485e7fb929b119254769a0f054
Laura Mora <laumormul@alu.edu.gva.es>
2026-04-10 13:39:57 +0200

    Get by id funcional

---
+++ backend/app/Taxes/Infrastructure/Entrypoint/Http/GetTaxByIDController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Taxes\Infrastructure\Entrypoint\Http;
+
+use App\Taxes\Application\GetTaxByID\GetTaxByID;
+use Illuminate\Http\JsonResponse;
+
+class GetTaxByIDController
+{
+    public function __construct(
+        private GetTaxByID $getTaxByID,
+    )
+    {}
+
+    public function __invoke(string $id)
+    {
+        $response = ($this->getTaxByID)($id);
+        if($response == null) {
+            return new JsonResponse('Family not found', 404);
+        } else {
+            return new JsonResponse($response->toArray(), 200);
+        }
+        
+    }
+}
