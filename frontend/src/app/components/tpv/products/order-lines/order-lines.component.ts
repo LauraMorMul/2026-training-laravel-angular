4e52f58776289bdcdf96af33601b31999e279ed2
Laura <lauramoramuleroinformatica@gmail.com>
2026-05-25 17:17:13 +0200

    Establecida base de creación de líneas de pedido

--- frontend/src/app/components/tpv/products/order-lines/order-lines.component.ts
+++ frontend/src/app/components/tpv/products/order-lines/order-lines.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { IOrderLines } from 'src/app/models/order_line';
+import { OrderLineManagerService } from 'src/app/services/tpv/order-line-manager-service';
 
 @Component({
   selector: 'app-order-lines',
   templateUrl: './order-lines.component.html',
   styleUrls: ['./order-lines.component.scss'],
 })
-export class OrderLinesComponent  implements OnInit {
+export class OrderLinesComponent implements OnInit {
+  private orderLineManager = inject(OrderLineManagerService);
 
-  constructor() { }
+  orderLines: IOrderLines = [];
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getOrderLines();
+  }
 
+  getOrderLines() {
+    this.orderLineManager.lines$.subscribe(lines => {
+      this.orderLines = lines;
+    });
+  }
+
+  removeLine(index: number) {
+    this.orderLineManager.removeOrderLine(index);
+  }
 }
--- frontend/src/app/components/tpv/products/product-container/product-container.component.ts
+++ frontend/src/app/components/tpv/products/product-container/product-container.component.ts
@@ -1,10 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductListComponent } from "../product-list/product-list.component";
+import { OrderLinesComponent } from "../order-lines/order-lines.component";
 
 @Component({
   selector: 'app-product-container',
   templateUrl: './product-container.component.html',
   styleUrls: ['./product-container.component.scss'],
+  imports: [ProductListComponent, OrderLinesComponent],
 })
 export class ProductContainerComponent  implements OnInit {
 private route = inject(ActivatedRoute);
@@ -14,7 +17,6 @@ private route = inject(ActivatedRoute);
   constructor() { }
 
   ngOnInit() {
-    console.log(this.tableId)
   }
 
 }
